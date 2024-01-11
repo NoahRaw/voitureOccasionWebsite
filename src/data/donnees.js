d3096486b7acb898852f6231e5fb07f850fca8e6
Fyantra <andrimalalafyantra@gmail.com>
2024-01-11 22:38:19 +0300

    Update donnees.js

--- src/data/donnees.js
+++ src/data/donnees.js
@@ -58,6 +58,60 @@
           type : 'text',
         }          
       ]
+    },
+    {
+      formulaireId : 'carburant',
+      formulaireTitle : 'Insertion de carburant',
+      jsonValue : 
+      {
+        // idCarburant: 0,
+        description: '',
+      },
+      lien : 'http://localhost:52195/Carburant/create',
+      listInput : 
+      [
+        {
+          name : 'description',
+          type : 'text',
+        }       
+      ]
+    },
+    {
+      formulaireId : 'comission',
+      formulaireTitle : 'Insertion de comission',
+      jsonValue : 
+      {
+        date : '',
+        pourcentage : 0,
+      },
+      lien : 'http://localhost:52195/Commission/create',
+      listInput : 
+      [
+        {
+          name : 'date',
+          type : 'date',
+        },  
+        {
+          name : 'pourcentage',
+          type : 'number',
+        }
+      ]
+    },
+    {
+      formulaireId : 'marque',
+      formulaireTitle : 'Insertion de marque',
+      jsonValue : 
+      {
+        description : "",
+      },
+      lien : 'http://localhost:52195/Marque/create',
+      listInput : 
+      [
+        {
+          name : 'description',
+          type : 'text',
+        }
+      ]
     }
   ]
 
