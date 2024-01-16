8d13cd071a61350f53da85b41d9e64e5a0777a2c
thonyETU1883 <thonyrasolofondraibe1883@gmail.com>
2024-01-16 23:32:31 +0100

    annonce

--- src/composant/annonce/AllAnnonce.js
+++ src/composant/annonce/AllAnnonce.js
@@ -1,50 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import DetailAnnonce from './DetailAnnonce';
 
-const UserDataComponent = () => {
-    const [userData, setUserData] = useState([]);
-    
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await fetch(`http://localhost:52195/Voitureutilisateur_view`);
-    
-            if (response.ok) {
-              const data = await response.json();
-              setUserData(data); // Mettez à jour l'état avec les données récupérées
-            } else {
-              console.error('Erreur lors de la requête HTTP:', response.statusText);
-            }
-          } catch (error) {
-            console.error('Erreur lors de la requête HTTP:', error);
-          }
-        };
-    
-        fetchData();
-      }, []);
+const AllAnnonce = () => {
+  const [userData, setUserData] = useState([]);
+  const [key, setKey] = useState(0);
 
-      return (
-        <div>
-          {userData.map((user) => (
-                <DetailAnnonce
-                idvoitureutilisateur={user.idvoitureutilisateur}
-                dateventedebut={user.dateventedebut}
-                matricule={user.matricule}
-                kilometrage={user.kilometrage}
-                prix={user.prix}
-                nommarque={user.nommarque}
-                nommodele={user.nommodele}
-                nomcarburant={user.nomcarburant}
-                kw={user.kw}
-                cv={user.cv}
-                nomboitedevitesse={user.nomboitedevitesse}
-                nomtypedevehicule={nomtypedevehicule}
-                nbrporte={nbrporte}
-                puissance={puissance}
-                />
-            ))}
-        </div>
-      );
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:52195/Voitureutilisateur_view`);
+
+        if (response.ok) {
+          const data = await response.json();
+          setUserData(data); // Mettez à jour l'état avec les données récupérées
+        } else {
+          console.error('Erreur lors de la requête HTTP:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Erreur lors de la requête HTTP:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+  return (
+    <div>
+      {userData.map((user) => (
+        <DetailAnnonce
+          key={user.idvoitureutilisateur}
+          idvoitureutilisateur={user.idvoitureutilisateur}
+          dateventedebut={user.dateventedebut}
+          matricule={user.matricule}
+          kilometrage={user.kilometrage}
+          prix={user.prix}
+          nommarque={user.nommarque}
+          nommodele={user.nommodele}
+          nomcarburant={user.nomcarburant}
+          kw={user.kw}
+          cv={user.cv}
+          nomboitedevitesse={user.nomboitedevitesse}
+          nomtypedevehicule={user.nomtypedevehicule}
+          nbrporte={user.nbrporte}
+          puissance={user.puissance}
+          setUserData={setUserData}
+        />
+      ))}
+    </div>
+  );
 };
 
-export default UserDataComponent;
+export default AllAnnonce;
--- src/composant/annonce/DetailAnnonce.js
+++ src/composant/annonce/DetailAnnonce.js
@@ -1,38 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function DetailAnnonce({idvoitureutilisateur,dateventedebut, matricule , kilometrage , prix ,nommarque ,nommodele,nomcarburant,kw,cv,nomboitedevitesse,nomtypedevehicule,nbrporte,puissance}) {
+export default function DetailAnnonce({ idvoitureutilisateur, dateventedebut, matricule, kilometrage, prix, nommarque, nommodele, nomcarburant, kw, cv, nomboitedevitesse, nomtypedevehicule, nbrporte, puissance, setUserData }) {
 
-    const handleSubmit = async () => {
+  const handleSubmit = async () => {
+    // Vous pouvez maintenant effectuer votre appel à votre service Spring Boot ici
+    try {
+      const response = await fetch(`http://localhost:52195/VoitureUtilisateurs/validation/${idvoitureutilisateur}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+
+        }),
+      });
+
+      if (response.ok) {
+        // Gérez le succès de l'appel ici
+        console.log('Données soumises avec succès');
+
+        const fetchData = async () => {
+          try {
+            const response = await fetch(`http://localhost:52195/Voitureutilisateur_view`);
     
-        // Vous pouvez maintenant effectuer votre appel à votre service Spring Boot ici
-        try {
-          const response = await fetch(`http://localhost:52195/VoitureUtilisateurs/validation/${idvoitureutilisateur}`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-               
-            }),
-          });
-    
-          if (response.ok) {
-            // Gérez le succès de l'appel ici
-            console.log('Données soumises avec succès');
-            window.location.reload();
-          } else {
-            console.error('Erreur lors de la soumission des données:', response.statusText);
+            if (response.ok) {
+              const data = await response.json();
+              setUserData(data); // Mettez à jour l'état avec les données récupérées
+            } else {
+              console.error('Erreur lors de la requête HTTP:', response.statusText);
+            }
+          } catch (error) {
+            console.error('Erreur lors de la requête HTTP:', error);
           }
-        } catch (error) {
-          console.error('Erreur lors de la soumission des données:', error);
-        }
-    };
+        };
+    
+        fetchData();
+        
+      } else {
+        console.error('Erreur lors de la soumission des données:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Erreur lors de la soumission des données:', error);
+    }
+  };
 
-    const [estVisible, setEstVisible] = useState(false);
+  const [estVisible, setEstVisible] = useState(false);
 
-    const handleClick = () => {
-      setEstVisible(!estVisible); 
-    };
+  const handleClick = () => {
+    setEstVisible(!estVisible);
+  };
 
   return (
     <div className="">
@@ -44,15 +60,14 @@ export default function DetailAnnonce({idvoitureutilisateur,dateventedebut, matr
         <p>Modele : {nommodele}</p>
         <p>Prix : {prix}</p>
 
-
         {estVisible && <div>
-            <p>carburant : {nomcarburant}</p>
-            <p>kw : {kw}</p>
-            <p>cv : {cv}</p>
-            <p>boite de vitesse : {nomboitedevitesse}</p>
-            <p>type de vehicule : {nomtypedevehicule}</p>
-            <p>nombre de porte : {nbrporte}</p>
-            <p>puissance : {puissance}</p>
+          <p>carburant : {nomcarburant}</p>
+          <p>kw : {kw}</p>
+          <p>cv : {cv}</p>
+          <p>boite de vitesse : {nomboitedevitesse}</p>
+          <p>type de vehicule : {nomtypedevehicule}</p>
+          <p>nombre de porte : {nbrporte}</p>
+          <p>puissance : {puissance}</p>
         </div>}
 
         <button onClick={handleClick}>detail</button>
