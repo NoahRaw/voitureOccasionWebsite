8e3e2189b4822145c7b0d404201de9edeb17bf13
Razrodrigue <119799092+Razrodrigue@users.noreply.github.com>
2024-01-29 18:55:22 +0200

    commit modification

--- src/composant/tableau/StatistiqueVente.js
+++ src/composant/tableau/StatistiqueVente.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-//import BarChart from "../chart/BarChart";
+import BarChart from "../chart/BarChart";
 
 const UserDataComponent = ({date}) => {
   const [userData, setUserData] = useState([]);
 
-  //const [chartData, setChartData] = useState(null);
+  const [chartData, setChartData] = useState(null);
 
   useEffect(() => {
     console.log(`${date.dateDebut} ou ${date.dateFin}`);
@@ -15,24 +15,24 @@ const UserDataComponent = ({date}) => {
         if (response.ok) {
           const data = await response.json();
           setUserData(data); // Mettez à jour l'état avec les données récupérées
-          //setChartData({
-          //  labels: data.map((data) => data.dateVente),
-          //  datasets: [
-          //  {
-          //      label: "Statistique commission",
-          //      data: data.map((data) => data.comission),
-          //      backgroundColor: [
-           //     "rgba(75,192,192,1)",
-          //      "#ecf0f1",
-           //     "#50AF95",
-           //     "#f3ba2f",
-           //     "#2a71d0",
-            //    ],
-            //    borderColor: "black",
-            //    borderWidth: 2,
-           // },
-           // ],
-           //})
+          setChartData({
+           labels: data.map((data) => data.dateVente),
+           datasets: [
+           {
+               label: "Statistique commission",
+               data: data.map((data) => data.comission),
+               backgroundColor: [
+               "rgba(75,192,192,1)",
+               "#ecf0f1",
+               "#50AF95",
+               "#f3ba2f",
+               "#2a71d0",
+               ],
+               borderColor: "black",
+               borderWidth: 2,
+           },
+           ],
+           })
         } else {
           console.error('Erreur lors de la requête HTTP:', response.statusText);
         }
@@ -49,8 +49,8 @@ const UserDataComponent = ({date}) => {
       <h1>Statistique commission</h1>
       <div style={{ width: 700 }}>
       {
-        //chartData &&
-        //<BarChart chartData={chartData} />
+        chartData!=null &&
+        <BarChart chartData={chartData} />
       }
       </div>
 
